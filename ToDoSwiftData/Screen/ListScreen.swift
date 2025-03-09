ded61e7ce5310d76b4f833e7c4bcc1b8a2b1473d
Ömer Apaydın <omerapaydin90@gmail.com>
2025-03-09 18:06:06 +0300

    Veri Çekmek

---
+++ ToDoSwiftData/Screen/ListScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ListScreen.swift
+//  ToDoSwiftData
+//
+//  Created by Ömer on 9.03.2025.
+//
+
+import SwiftUI
+
+struct ListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListScreen()
+}
