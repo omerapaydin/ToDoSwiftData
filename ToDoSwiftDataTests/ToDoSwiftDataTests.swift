d93e9edb09c8aa6f650e6ebe2a26d8ba3283023a
Ömer Apaydın <omerapaydin90@gmail.com>
2026-04-20 01:52:32 +0300

    Initial Commit

---
+++ ToDoSwiftDataTests/ToDoSwiftDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoSwiftDataTests.swift
+//  ToDoSwiftDataTests
+//
+//  Created by Ömer on 20.04.2026.
+//
+
+import Testing
+@testable import ToDoSwiftData
+
+struct ToDoSwiftDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
