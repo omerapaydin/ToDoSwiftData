95eff72f8dfe4258b36b68283140fd737f5b8982
Ömer Apaydın <omerapaydin90@gmail.com>
2026-04-21 00:11:14 +0300

    sheet

---
+++ ToDoSwiftData/View/ToDoListView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListView.swift
+//  ToDoSwiftData
+//
+//  Created by Ömer on 20.04.2026.
+//
+
+import SwiftUI
+
+struct ToDoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoListView()
+}
